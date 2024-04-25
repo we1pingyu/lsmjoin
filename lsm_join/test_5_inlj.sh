83ca5ca26f9c2ee97a5b188075488925e9a24d52
we1pingyu <380376987@qq.com>
2024-04-25 12:01:33 +0800

    fix

--- lsm_join/test_5_inlj.sh
+++ lsm_join/test_5_inlj.sh
@@ -11,6 +11,7 @@ num_loops=(1)
 for dataset in "${datasets[@]}"; do
     epsilon=0.1
     k=1
+    skew_flag=""
     if [ "$dataset" == "movie_id" ]; then
         public_data_flag="--public_data"
         public_r="${data_path}movie_info_movie_id"
@@ -30,22 +31,23 @@ for dataset in "${datasets[@]}"; do
     else
         public_data_flag=""
         epsilon=0.9
-        k=9
+        k=4
+        skew_flag="--skew"
     fi
     output="${dataset}_5nlj.txt"
     rm -f $output
     for num_loop in "${num_loops[@]}"; do
         #index nested loop join
-        echo "./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="Eager" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k"
-        ./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="Eager" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k
-        ./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="Lazy" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k
-        echo "./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="Comp" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k"
-        ./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="Comp" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k
-        ./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="CEager" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k
-        ./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="CLazy" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k
-        ./exp_runner --M=64 --B=128 --J="INLJ" --r_index="Regular" --s_index="CComp" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k
+        echo "./exp_runner --J="INLJ" --r_index="Regular" --s_index="Eager" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag"
+        ./exp_runner --J="INLJ" --r_index="Regular" --s_index="Eager" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag
+        ./exp_runner --J="INLJ" --r_index="Regular" --s_index="Lazy" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag
+        echo "./exp_runner --J="INLJ" --r_index="Regular" --s_index="Comp" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag"
+        ./exp_runner --J="INLJ" --r_index="Regular" --s_index="Comp" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag
+        ./exp_runner --J="INLJ" --r_index="Regular" --s_index="CEager" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag
+        ./exp_runner --J="INLJ" --r_index="Regular" --s_index="CLazy" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag
+        ./exp_runner --J="INLJ" --r_index="Regular" --s_index="CComp" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag
         # hash join
-        echo "./exp_runner --M=64 --B=128 --J="HJ" --r_index="Regular" --s_index="Regular" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k"
-        ./exp_runner --M=64 --B=128 --J="HJ" --r_index="Regular" --s_index="Regular" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --epsilon=$epsilon --k=$k
+        echo "./exp_runner --J="HJ" --r_index="Regular" --s_index="Regular" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag"
+        ./exp_runner --J="HJ" --r_index="Regular" --s_index="Regular" $public_data_flag --public_r=$public_r --public_s=$public_s --num_loop=$num_loop --output_file=$output --db_r=$db_r_path --db_s=$db_s_path --ingestion --eps_s=$epsilon --k_s=$k --k_r=$k $skew_flag
     done
 done
